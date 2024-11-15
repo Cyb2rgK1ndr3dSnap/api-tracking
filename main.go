aac13a1d602326024afd7d069c4439767238e258
Rafael <rafael135zk@gmail.com>
2024-11-14 22:03:43 -0500

    fix.

--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"log"
 	"os"
 
 	"github.com/Cyb2rgK1ndr3dSnap/api-tracking/initializers"
 	"github.com/Cyb2rgK1ndr3dSnap/api-tracking/routes"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	port := os.Getenv("PORT")
 
 	db := initializers.InitDB()
